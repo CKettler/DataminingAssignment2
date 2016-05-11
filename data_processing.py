f8565e1a836b5436a5444aed6df7e30933d8852a
CKettler <celestekettler@gmail.com>
2016-05-11 16:26:11 +0200

    small change in data aggregation to be able to do the correlation calculation correctly

--- data_processing.py
+++ data_processing.py
@@ -10,7 +10,7 @@ class DataAggregator:
         self.variables = list(self.df.columns.values)  # all possible variable names
         print "data opened"
 
-    def read(self, method='some'):
+    def read(self, ):
         """
         Generates the aggregated data.
         There are a few possible variables.
@@ -20,7 +20,11 @@ class DataAggregator:
 
         if method == 'some':
             return self.window_some_features()
+        elif method == 'cor':
+            targets = [self.df['click_bool'], self.df['booking_bool']]
+            return self.df, self.variables, targets
 
+    method = 'cor'
     def all_data(self):
         data_frames_list = []
         targets = []
