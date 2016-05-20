754d35d878ea44334c067f873471f95585a87710
CKettler <celestekettler@gmail.com>
2016-05-20 13:28:56 +0200

    adapting main so if I run it I will be able to classify on the dummy

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ main run file
 """
 
 import data_preprocessing as dp
+import data_aggregator as da
 from datetime import datetime
+import dummy_classifier as dc
 import numpy as np
 import csv
 import pandas as pd
@@ -15,12 +17,24 @@ filepath = 'data/data_slice_1.csv'
 #filepath = 'data/mega_small.csv'
 
 
-data_aggregator = dp.DataPreprocessing(filepath)
+#data_aggregator = dp.DataPreprocessing(filepath)
 
-data_aggregator.add_data()
+#data_aggregator.add_data()
 
-print 'data added', datetime.now().time()
+#print 'data added', datetime.now().time()
 
-data_aggregator.df.to_csv("data/data_slice_1_added_variables.csv")
+#data_aggregator.df.to_csv("data/data_slice_1_added_variables.csv")
 
-print 'data saved', datetime.now().time()
+#print 'data saved', datetime.now().time()
+
+filepath2 = 'data/data_slice_1_added_variables.csv'
+
+data_aggregator = da.DataAggregator(filepath2)
+df = data_aggregator.read_data()
+print variables = list(df.columns.values)
+target = data_aggregator.df['target'].as_matrix()
+print target
+# df = data_aggregator.keep_df_variables()
+#
+#
+# dummy_classifier = dc.(df, targets, test_df)
