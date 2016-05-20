1af77ec589d580f40edc0e2439af2348437677ed
Erik van Egmond <erikvane@gmail.com>
2016-05-20 15:12:48 +0200

    Added wrapper for query

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,7 +13,7 @@ class DataPreprocessing(DataAggregator):
 
         print "data opened", datetime.now().time()
 
-    def read(self, method = 'some'):
+    def read(self, method='some'):
         """
         Generates the aggregated data.
         There are a few possible variables.
@@ -27,21 +27,20 @@ class DataPreprocessing(DataAggregator):
             targets = [self.df['click_bool'], self.df['booking_bool'], self.df['target']]
             return self.df, self.variables, targets
 
-
     def add_data(self):
 
         lambdafunc = lambda x: pd.Series([x['date_time'].hour / 6,
-                                         self.comp_rate_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
-                                         self.comp_expensive_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
-                                         self.comp_cheap_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
-                                         int(x['click_bool']) + 5 * int(x['booking_bool'])
-                                         ])
+                                          self.comp_rate_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
+                                          self.comp_expensive_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
+                                          self.comp_cheap_calc([x['comp%d_rate' % i] for i in range(1, 9)]),
+                                          int(x['click_bool']) + 5 * int(x['booking_bool'])
+                                          ])
 
         print "lambdafunc created", datetime.now().time()
         newcols = self.df.apply(lambdafunc, axis=1)
         print "new cols created", datetime.now().time()
         newcols.columns = ['time_class', 'comp_rate_sum', 'comp_expensive', 'comp_cheap', 'target']
-        booking_properties_dict =  self.bookings_property()
+        booking_properties_dict = self.bookings_property()
         booking_prop_array, found_prop_array = self.feature_from_booking_properties(booking_properties_dict)
         self.df['no_bookings_prop'] = np.transpose(booking_prop_array)
         self.df['no_found_prop'] = np.transpose(found_prop_array)
@@ -60,12 +59,11 @@ class DataPreprocessing(DataAggregator):
         max = np.max(comps_list)
         return 1 if max > 0 else 0
 
-
     def bookings_property(self):
         # creates dictionary of hotels, returning 123(number hotel): 3(number counts)
         booking_properties_dict = {}
         rows, cols = self.df.shape
-        for i in range(0,rows):
+        for i in range(0, rows):
             if self.df['booking_bool'][i] == 1:
                 property_id = self.df['prop_id'][i]
                 if str(property_id) in booking_properties_dict:
@@ -74,7 +72,6 @@ class DataPreprocessing(DataAggregator):
                     booking_properties_dict[str(property_id)] = 1
 
         return booking_properties_dict
-
 
     def feature_from_booking_properties(self, booking_properties_dict):
         rows, cols = self.df.shape
@@ -89,5 +86,5 @@ class DataPreprocessing(DataAggregator):
                 feature_array_found[index] = no_times_found
         return feature_array_bookings, feature_array_found
 
-
-
+    def filter(self, query):
+        return self.df.query(query)
