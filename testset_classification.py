9ac16c9ab3a2066e9bf6a14ee4f07714ee2d1381
Erik <erikvane@gmail.com>
2016-05-31 23:11:05 +0200

    some optimizations in classification and combined classfication and add vars in testset_run_all

--- testset_classification.py
+++ testset_classification.py
@@ -10,25 +10,25 @@ def make_X(testdf, select_cols):
     X = np.nan_to_num(X)
     return X
 
+print "start classfieing"
 
-for i in range(13, 16):
+model = pkl.load(open(
+    'Classifiers_result_2\gradient_boosting_Boosting-False_max_leaf_nodes-4-learning_rate-0.1-n_estimators-100-subsample-0.5-random_state-2-min_samples_split-5-max_depth-None.pkl',
+    'r'))
+
+select_cols = ['prop_starrating', 'prop_review_score', 'prop_location_score2', 'price_usd',
+               'promotion_flag', 'no_bookings_prop', 'no_found_prop']
+rank_options = [False]
+
+for i in slices_to_do:
     data_file = "data/test_set_added_variables_%i.csv" % (i)
     data_test_slice = dp.DataAggregator(data_file)
     data_test_df = data_test_slice.df
 
-    select_cols = ['prop_starrating', 'prop_review_score', 'prop_location_score2', 'position', 'price_usd',
-                   'promotion_flag', 'no_bookings_prop', 'no_found_prop']
-
     X_test = make_X(data_test_df, select_cols)
-
-    model = pkl.load(open(
-        'Classifiers_result_2\gradient_boosting_Boosting-False_max_leaf_nodes-4-learning_rate-0.1-n_estimators-100-subsample-0.5-random_state-2-min_samples_split-5-max_depth-None.pkl',
-        'r'))
 
     y_pred = model.predict(X_test)
     y_prob = model.predict_proba(X_test)
-
-    rank_options = [False]
 
     for preshuffle in rank_options:
         df_with_ranking = rk.ranking(data_test_df, y_pred, y_prob, preshuffle=preshuffle, target=False)
@@ -36,3 +36,4 @@ for i in range(13, 16):
     final_df = df_with_ranking[['srch_id', 'prop_id']]
 
     final_df.to_csv('prediction_file%d.csv' % (i), index=False)
+    print "slice %d done" % (i)
