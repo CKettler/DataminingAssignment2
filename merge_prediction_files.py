e9bcfb6eb197341271ba0cac4108cfc65d622f4e
CKettler <celestekettler@gmail.com>
2016-06-07 11:26:50 +0200

    commented some things out

--- merge_prediction_files.py
+++ merge_prediction_files.py
@@ -3,10 +3,13 @@ import glob
 
 path =r'data/pred_files' # use your path
 allFiles = glob.glob(path + "/*.csv")
+
+
+print allFiles
 frame = pd.DataFrame()
 list_ = []
 for file_ in allFiles:
     df = pd.read_csv(file_,index_col=None, header=0)
     list_.append(df)
 frame = pd.concat(list_)
-frame.to_csv('data/prediction_file.csv')
+frame.to_csv('data/prediction_file.csv', index=False)
